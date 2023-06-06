fb7d6c709f1b89d33e6a4ed47434734a33f4fecb
chris <reichel32574@gmail.com>
2023-06-07 00:03:59 +0200

    Initial commit

---
+++ src/main/java/com/github/maheevil/template/TemplatePlugin.java
@@ -0,0 +1,15 @@
+package com.github.maheevil.template;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TemplatePlugin extends JavaPlugin {
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
